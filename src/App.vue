ea7dd73044f711f41be5140808cef9afeccde7dd
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-10-07 18:11:48 -0600

    Add animations between pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="app" class="mt3 tc">
-    <router-view></router-view>
+  <div id="app" class="tc">
+    <div class="w100 bg-vue pa1"></div>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      transitionName: 'slide-left'
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    const toDepth = to.path.split('/').length
+    const fromDepth = from.path.split('/').length
+    this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+    next()
+  }
 }
 </script>
 
