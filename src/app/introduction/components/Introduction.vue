9ea96e0ef20ee8cf6bca7cfa0f0cbe83d48b4e8a
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-10-07 19:07:42 -0600

    Change README

--- src/app/introduction/components/Introduction.vue
+++ src/app/introduction/components/Introduction.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-      <h1 class="f1 blue">Introduction</h1>
+    <div class="dt tc center mv4">
+      <router-link to="/">
+        <img src="~@/assets/logo.png" class="dib v-mid h3 dtc mr4">
+      </router-link>
+      <h1 class="blue f1 v-mid dtc">Introduction /</h1>
+      <h2 class="green f2 v-mid dtc">About Vue.js</h2>
+    </div>
   </div>
 </template>
 
