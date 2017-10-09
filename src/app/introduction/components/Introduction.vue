73420e81a4c328e02561982fbcf2a967852253f9
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-10-08 19:59:44 -0600

    Add Angular 2 comparison

--- src/app/introduction/components/Introduction.vue
+++ src/app/introduction/components/Introduction.vue
@@ -32,6 +32,29 @@
         <hr class="mw3 bb bw1 b--black-10">
         <h4 class="f4">Directives vs Components</h4>
         <p class="lh-copy f5 black-70 tl tj">Vue has a clearer separation between directives and components. Directives are meant to encapsulate DOM manipulations only, while components are self-contained units that have their own view and data logic. In Angular, there’s a lot of confusion between the two.</p>
+        <hr class="mw3 bb bw1 b--black-10">
+        <h4 class="f4">Performance</h4>
+        <p class="lh-copy f5 black-70 tl tj">Vue has better performance and is much, much easier to optimize because it doesn’t use dirty checking. Angular 1 becomes slow when there are a lot of watchers, because every time anything in the scope changes, all these watchers need to be re-evaluated again. Also, the digest cycle may have to run multiple times to “stabilize” if some watcher triggers another update. Angular users often have to resort to esoteric techniques to get around the digest cycle, and in some situations, there’s simply no way to optimize a scope with many watchers.</p>
+        <p class="lh-copy f5 black-70 tl tj">Vue doesn’t suffer from this at all because it uses a transparent dependency-tracking observation system with async queueing - all changes trigger independently unless they have explicit depen- dency relationships.
+        Interestingly, there are quite a few similarities in how Angular 2 and Vue are addressing these Angular 1 issues.</p>
+      </article>
+      <article class="mw8 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <div class="tc">
+          <img src="/static/logos/angular.png" class="h4 dib" title="Angular 1">
+          <h3 class="f3">Angular 2</h3>
+        </div>
+        <p class="lh-copy center f5 black-70 tl tj">There is a separate section for Angular 2 because it really is a completely new framework. For example, it features a first-class component system, many implementation details have been completely rewritten, and the API has also changed quite drastically.</p>
+        <hr class="mw3 bb bw1 b--black-10">
+        <h4 class="f4 fw6">Size and Performance</h4>
+        <p class="lh-copy f5 black-70 tl tj">In terms of performance, both frameworks are exceptionally fast and there isn’t enough data from real world use cases to make a verdict. However if you are determined to see some numbers, Vue 2.0 seems to be ahead of Angular 2 according to this 3rd party benchmark.</p>
+        <p class="lh-copy f5 black-70 tl tj">Size wise, although Angular 2 with offline compilation and tree-shaking is able to get its size down considerably, a full-featured Vue 2.0 with compiler included (23kb) is still lighter than a tree-shaken bare-bone example of Angular 2 (50kb).</p>
+        <hr class="mw3 bb bw1 b--black-10">
+        <h4 class="f4">Flexibility</h4>
+        <p class="lh-copy f5 black-70 tl tj">Vue is much less opinionated than Angular 2, offering official support for a variety of build systems, with no restrictions on how you structure your application. Many developers enjoy this freedom, while some prefer having only one Right Way to build any application.</p>
+        <hr class="mw3 bb bw1 b--black-10">
+        <h4 class="f4">Learning Curve</h4>
+        <p class="lh-copy f5 black-70 tl tj">To get started with Vue, all you need is familiarity with HTML and ES5 JavaScript (i.e. plain JavaScript). With these basic skills, you can start building non-trivial applications within less than a day of reading the guide.</p>
+        <p class="lh-copy f5 black-70 tl tj">Angular’s learning curve is much steeper. The API surface of the framework is simply huge and as a user you will need to familiarize yourself with a lot more concepts before getting productive. Obviously, the complexity of Angular is largely due to its design goal of targeting only large, complex applications - but that does make the framework a lot more difficult for less-experienced developers to pick up.</p>        
       </article>
     </div>
     </div>
